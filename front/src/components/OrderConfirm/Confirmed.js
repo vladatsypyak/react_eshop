3ef4d267299c1b85aeb3f8dc544fbb6b3b50b982
Vladyslava <vladatsyupyak@gmail.com>
2023-08-20 19:31:32 +0300

    style: confirmed

--- front/src/components/OrderConfirm/Confirmed.js
+++ front/src/components/OrderConfirm/Confirmed.js
@@ -3,6 +3,7 @@ import s from "./orderConfirm.module.scss"
 import {Link} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
 import {Modal} from "../Auth/Modal";
+import {GlobalSvgSelector} from "../../assets/GlobalSvgSelector";
 
 export const Confirmed = () => {
     const orderData = useSelector(state => state.cart.orderData)
@@ -10,6 +11,10 @@ export const Confirmed = () => {
     return <div className={s.order_confirm}>
         <Modal>
             <div className={s.info_wrap}>
+                <div className={s.done_img}>
+                    <GlobalSvgSelector id={"done"}/>
+
+                </div>
                <p>Дякуємо за замовлення!</p>
                 <p>З подальшими питаннями з вами звяжеться менеджер</p>
 
