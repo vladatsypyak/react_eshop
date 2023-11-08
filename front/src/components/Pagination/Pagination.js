32ba242d89360a1c15ff78167fd11b40dbce3f96
Vladyslava <vladatsyupyak@gmail.com>
2023-11-08 14:37:06 +0200

    fix: dont show pagination when only 1 page

--- front/src/components/Pagination/Pagination.js
+++ front/src/components/Pagination/Pagination.js
@@ -21,33 +21,36 @@ export const Pagination = () => {
         let p = page === pageCount ? page : page + 1
         dispatch(setPage(p))
     }
-    return (
-        <nav className={s.pagination}>
-            <ul className={"pagination justify-content-center"}>
-                <li onClick={handlePrevious} className={page === 1 ? "page-item disabled" : "page-item"}>
-                    <a className="page-link" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a>
-                </li>
-                {Array(pageCount)
-                    .fill(null)
-                    .map((_, index) => {
-                        return <li
-                            onClick={() => {
-                                dispatch(setPage(index + 1))
-                                console.log(page)
-                                console.log(index)
-                            }}
-                            className={page === index + 1  ? `page-item active ${s.active}` : "page-item"}
-                            key={index}>
-                            <a className={"page-link"}>{index + 1}</a>
-                        </li>;
-                    })}
-                <li onClick={handleNext} className={page === pageCount ? "page-item disabled" : "page-item"}>
-                    <a className="page-link"><span aria-hidden="true">&raquo;</span></a>
-                </li>
-            </ul>
 
-        </nav>
+    return (<>
+            {pageTotal && pageTotal > 1 && <nav className={s.pagination}>
+                <ul className={"pagination justify-content-center"}>
+                    <li onClick={handlePrevious} className={page === 1 ? "page-item disabled" : "page-item"}>
+                        <a className="page-link" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a>
+                    </li>
+                    {Array(pageCount)
+                        .fill(null)
+                        .map((_, index) => {
+                            return <li
+                                onClick={() => {
+                                    dispatch(setPage(index + 1))
+                                    console.log(page)
+                                    console.log(index)
+                                }}
+                                className={page === index + 1 ? `page-item active ${s.active}` : "page-item"}
+                                key={index}>
+                                <a className={"page-link"}>{index + 1}</a>
+                            </li>;
+                        })}
+                    <li onClick={handleNext} className={page === pageCount ? "page-item disabled" : "page-item"}>
+                        <a className="page-link"><span aria-hidden="true">&raquo;</span></a>
+                    </li>
+                </ul>
+
+            </nav>}
+        </>
+
     )
-
-
 }
+
+
