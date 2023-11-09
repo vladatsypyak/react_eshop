2aec7847fd1e55e3367db7f71e75b73df47070e4
Vladyslava <vladatsyupyak@gmail.com>
2023-11-09 12:52:48 +0200

    style: header responsive

--- front/src/components/Header/Header.js
+++ front/src/components/Header/Header.js
@@ -24,31 +24,45 @@ export const Header = () => {
     }
 
     return <header className={s.header}>
-        <div className={`wrapper ${s.header_container}`}>
-            <Link to={""}>
-                <div className={s.logo}>
-                    <img src={logo} alt=""/>
-                </div>
-            </Link>
-            <button ref={btnRef} onClick={onCatalogueBtnClick} className={s.catalogueBtn}>
-                <GlobalSvgSelector id="catalogue_icon"/>
-                <p className={s.catalogue_text}>Каталог</p>
-            </button>
-            <Search/>
-            <div className={s.icons}>
-                <div className={s.icon}>
-                    <Link to={"/favourites"}><img  src={heart} alt=""/></Link>
-                </div>
-                <div className={s.icon}>
-                   <Link to={"cart"}><GlobalSvgSelector id="cart_icon"/></Link>
-                    {/*<GlobalSvgSelector id="cart_icon"/>*/}
-                </div>
-                <div ref={userMenuBtn}  onClick={()=> setShowUserMenu(!showUserMenu)} className={s.icon}>
-                    <GlobalSvgSelector id="user_icon"/>
-                </div>
+        <nav className={`navbar navbar-expand-lg wrapper `} data-bs-theme="dark">
+         <div className={`${s.header_container} container-fluid `}>
+            <div className={"navbar-brand"}>
+                <Link to={""}>
+                    <div className={s.logo}>
+                        <img src={logo} alt=""/>
+                    </div>
+                </Link>
             </div>
-            {isCatalogue && <Catalogue btnRef={btnRef} hideCatalogue={hideCatalogue}/>}
-            <UserMenuPopup usermenuRef={userMenuBtn} setIsShown={setShowUserMenu} isShown={showUserMenu}/>
-        </div>
+             <button ref={btnRef} onClick={onCatalogueBtnClick} className={s.catalogueBtn}>
+                 <GlobalSvgSelector id="catalogue_icon"/>
+                 <p className={s.catalogue_text}>Каталог</p>
+             </button>
+
+             <button style={{border: "1px solid white"}} className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                     data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false"
+                     aria-label="Toggle navigation">
+                 <span  className="navbar-toggler-icon"></span>
+             </button>
+             <div className={` ${s.collapse} collapse navbar-collapse`} id="navbarToggler">
+                 <Search/>
+
+                 <ul className={`${s.icons} navbar-nav `}>
+                     <li className={`${s.icon} nav-item`}>
+                         <Link to={"/favourites"}><img  src={heart} alt=""/></Link>
+                     </li>
+                     <li className={`${s.icon} nav-item`}>
+                         <Link to={"cart"}><GlobalSvgSelector id="cart_icon"/></Link>
+                         {/*<GlobalSvgSelector id="cart_icon"/>*/}
+                     </li>
+                     <li ref={userMenuBtn}  onClick={()=> setShowUserMenu(!showUserMenu)} className={`${s.icon} nav-item`}>
+                         <GlobalSvgSelector id="user_icon"/>
+                     </li>
+                 </ul>
+             </div>
+         </div>
+        </nav>
+        {isCatalogue && <Catalogue btnRef={btnRef} hideCatalogue={hideCatalogue}/>}
+        <UserMenuPopup usermenuRef={userMenuBtn} setIsShown={setShowUserMenu} isShown={showUserMenu}/>
+
     </header>
 }
