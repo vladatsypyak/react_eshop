296bf3107ca0770a0927093546187c1c16f243da
Vladyslava <vladatsyupyak@gmail.com>
2023-11-29 23:44:15 +0200

    style: responsible modal window for auth

--- front/src/components/Auth/Modal.js
+++ front/src/components/Auth/Modal.js
@@ -4,6 +4,13 @@ import s from "./modal.module.scss"
 
 
 export const Modal = ({children}) => {
+
+    React.useEffect(() => {
+        document.body.style.overflow = "hidden";
+        return () => {
+            document.body.style.overflow = "scroll"
+        };
+    }, []);
     return <div className={s.modal}>
         <div className={s.overlay}></div>
        <div className={s.modal_wrap}>
