6e6920b5a314fe5437e0556543487d3ef4f56c2f
Vladyslava <vladatsyupyak@gmail.com>
2023-12-06 17:18:10 +0200

    fix:make modal auth with fixed position

--- front/src/components/Auth/Modal.js
+++ front/src/components/Auth/Modal.js
@@ -1,18 +1,21 @@
 import React from "react";
 import s from "./modal.module.scss"
+import {Overlay} from "../Overlay/Overlay";
 
 
 
 export const Modal = ({children}) => {
 
-    React.useEffect(() => {
-        document.body.style.overflow = "hidden";
-        return () => {
-            document.body.style.overflow = "scroll"
-        };
-    }, []);
+    // React.useEffect(() => {
+    //     document.body.style.overflow = "hidden";
+    //     return () => {
+    //         document.body.style.overflow = "scroll"
+    //     };
+    // }, []);
     return <div className={s.modal}>
-        <div className={s.overlay}></div>
+        <div className={s.overlay}>
+            <Overlay/>
+        </div>
        <div className={s.modal_wrap}>
            {children}
        </div>
