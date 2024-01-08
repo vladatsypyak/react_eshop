7e676f66b24d3602c51117f13b83a601b5405933
Vladyslava <vladatsyupyak@gmail.com>
2024-01-09 01:01:08 +0200

    style: fix button position header

--- front/src/components/Header/Header.js
+++ front/src/components/Header/Header.js
@@ -33,17 +33,19 @@ export const Header = () => {
     return <header className={s.header}>
         <nav className={`navbar navbar-expand-lg navbar-light wrapper `}>
             <div className={`${s.header_container} container-fluid `}>
-                <div className={`navbar-brand ${s.logo_wrap}`}>
-                    <Link to={""}>
-                        <div className={s.logo}>
-                            <img src={logo} alt=""/>
-                        </div>
-                    </Link>
-                </div>
-                <button ref={btnRef} onClick={onCatalogueBtnClick} className={s.catalogueBtn}>
-                    <GlobalSvgSelector id="catalogue_icon"/>
-                    <p className={s.catalogue_text}>Каталог</p>
-                </button>
+             <div className={s.left_wrap}>
+                 <div className={`navbar-brand ${s.logo_wrap}`}>
+                     <Link to={""}>
+                         <div className={s.logo}>
+                             <img src={logo} alt=""/>
+                         </div>
+                     </Link>
+                 </div>
+                 <button ref={btnRef} onClick={onCatalogueBtnClick} className={s.catalogueBtn}>
+                     <GlobalSvgSelector id="catalogue_icon"/>
+                     <p className={s.catalogue_text}>Каталог</p>
+                 </button>
+             </div>
 
                 <button style={{ border: "1px solid black" }
                 } className={`navbar-toggler ${s.custom_toggler}`} id={"button"} type="button"
@@ -54,23 +56,27 @@ export const Header = () => {
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className={` ${s.collapse} collapse navbar-collapse`} id="navbarTogglerDemo0">
-                    <Search/>
+                  <div className={s.search_wrap}>
+                      <Search/>
+                  </div>
 
-                    <ul className={`${s.icons} navbar-nav `}>
-                        <li className={`${s.icon} ${s.heart_icon} nav-item`}>
-                            <Link to={"/favourites"}> <GlobalSvgSelector id="heart_green"/></Link>
-                        </li>
-                        <li className={`${s.icon} ${s.cart_icon} nav-item`}>
-                            <Link to={"cart"}><GlobalSvgSelector id="cart_icon"/></Link>
-                            {/*<GlobalSvgSelector id="cart_icon"/>*/}
-                            {quantityInCart?.length > 0 && <QuantityInCart number={quantityInCart?.length}/>}
+                 <div className={s.icons_wrap}>
+                     <ul className={`${s.icons} navbar-nav `}>
+                         <li className={`${s.icon} ${s.heart_icon} nav-item`}>
+                             <Link to={"/favourites"}> <GlobalSvgSelector id="heart_green"/></Link>
+                         </li>
+                         <li className={`${s.icon} ${s.cart_icon} nav-item`}>
+                             <Link to={"cart"}><GlobalSvgSelector id="cart_icon"/></Link>
+                             {/*<GlobalSvgSelector id="cart_icon"/>*/}
+                             {quantityInCart?.length > 0 && <QuantityInCart number={quantityInCart?.length}/>}
 
-                        </li>
-                        <li ref={userMenuBtn} onClick={() => setShowUserMenu(!showUserMenu)}
-                            className={`${s.icon} ${s.user_icon} nav-item`}>
-                            <GlobalSvgSelector id="user_icon"/>
-                        </li>
-                    </ul>
+                         </li>
+                         <li ref={userMenuBtn} onClick={() => setShowUserMenu(!showUserMenu)}
+                             className={`${s.icon} ${s.user_icon} nav-item`}>
+                             <GlobalSvgSelector id="user_icon"/>
+                         </li>
+                     </ul>
+                 </div>
                 </div>
             </div>
         </nav>
