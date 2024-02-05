16197779a028cfe1e774e9fc8dd9553abdfad625
Vladyslava <vladatsyupyak@gmail.com>
2024-02-05 22:53:33 +0200

    feat: follow link when click on slide

--- front/src/components/Slider/Slider.js
+++ front/src/components/Slider/Slider.js
@@ -1,11 +1,27 @@
 import React from "react";
 import s from "./slider.module.scss"
 import {GlobalSvgSelector} from "../../assets/GlobalSvgSelector";
+import {useNavigate} from "react-router-dom";
 
 const imagesUrls = ["https://vlada-tea-images.s3.eu-north-1.amazonaws.com/%D0%B0%D0%BA%D1%86%D1%96%D1%8F3.png",
     "https://vlada-tea-images.s3.eu-north-1.amazonaws.com/%D0%B0%D0%BA%D1%86%D1%96%D1%8F5.jpg",
     "https://vlada-tea-images.s3.eu-north-1.amazonaws.com/%D0%B0%D0%BA%D1%86%D1%96%D1%8F.jpg"]
+const sliderImages = [
+    {
+        imgUrl: "https://vlada-tea-images.s3.eu-north-1.amazonaws.com/%D0%B0%D0%BA%D1%86%D1%96%D1%8F3.png",
+        link:`/categories/tea`
+    },
+    {
+        imgUrl: "https://vlada-tea-images.s3.eu-north-1.amazonaws.com/%D0%B0%D0%BA%D1%86%D1%96%D1%8F5.jpg",
+        link: `/categories/machines`
+    },
+    {
+        imgUrl: "https://vlada-tea-images.s3.eu-north-1.amazonaws.com/%D0%B0%D0%BA%D1%86%D1%96%D1%8F.jpg",
+        link: `items/654a21dd81cf89b949fea37c`
+    }
+]
 export const Slider = () => {
+    const navigate = useNavigate()
     const [slideIndex, setSlideIndex] = React.useState(0);
 
     function onPrevClick() {
@@ -28,6 +44,9 @@ export const Slider = () => {
     function onDotClick(i) {
         setSlideIndex(i)
     }
+    function onSlideClick(index){
+        navigate(sliderImages[index].link)
+    }
 
     return <div className={s.slider}>
         <div className={s.wrap}>
@@ -36,7 +55,7 @@ export const Slider = () => {
 
             </button>
             <div className={s.slide_image}>
-                <img src={imagesUrls[slideIndex]} alt=""/>
+                <img onClick={() =>onSlideClick(slideIndex)} src={sliderImages[slideIndex].imgUrl} alt=""/>
 
             </div>
             <button className={s.right} onClick={onNextClick}>
