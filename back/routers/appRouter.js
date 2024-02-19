68e87981fd32d0e38d3631ecb20a62072f082051
Vladyslava <vladatsyupyak@gmail.com>
2024-02-19 22:53:41 +0200

    fix: separate orders routes and add swagger

--- back/routers/appRouter.js
+++ back/routers/appRouter.js
@@ -1,40 +1,8 @@
 const express = require('express');
 
 const router = express.Router();
-// const {authMiddleware} = require('../middleware/authMiddleware');
-const {
-     getItemsByCategory, getCategoryFilters, getFilterValues, getFilteredItems,
-    addToFavourite, getFavourites, deleteFavourite, getItemById, getUserFavourites,
-    getPriceRange
-} = require("../controllers/appService");
-const {searchCategories, getCategories, getCategoryByType} = require("../controllers/categoriesService");
-const {removeOneFromCart, getUserCartItems, addToCart, deleteCartItem, clearCart} = require("../controllers/cartService");
 const {createOrder, getUserOrders, deleteUserOrder} = require("../controllers/ordersService");
 const {authMiddleware} = require("../middleware/authMiddleware");
-
-
-
-
-
-router.post('/app/favourite', authMiddleware, addToFavourite);
-router.get('/app/favourite', authMiddleware, getUserFavourites);
-router.delete('/app/favourite',authMiddleware, deleteFavourite);
-
-router.post('/app/cart/remove', removeOneFromCart);
-router.get('/app/cart/:userId', getUserCartItems);
-router.post('/app/cart', addToCart);
-router.delete('/app/cart', deleteCartItem)
-router.delete('/app/cart/clear/:userId', clearCart);
-
-
-router.post('/app/orders/add', authMiddleware, createOrder);
-router.get('/app/orders/:userId', authMiddleware, getUserOrders);
-router.delete('/app/orders', authMiddleware, deleteUserOrder)
-
-
-
-
-
 
 
 
--- back/routers/ordersRouter.js
+++ back/routers/ordersRouter.js
@@ -3,9 +3,85 @@ const {authMiddleware} = require("../middleware/authMiddleware");
 const {createOrder, getUserOrders, deleteUserOrder} = require("../controllers/ordersService");
 const router = express.Router();
 
-router.post('/app/orders/add', authMiddleware, createOrder);
-router.get('/app/orders/:userId', authMiddleware, getUserOrders);
-router.delete('/app/orders', authMiddleware, deleteUserOrder)
+/**
+ * @swagger
+ * /api/orders:
+ *   post:
+ *     tags:
+ *       - orders
+ *     summary: Create new order
+ *     description: Create a new order
+ *     security:
+ *       - BearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               items:
+ *                 type: array
+ *                 description: array of items
+ *               price:
+ *                 type: string
+ *                 description: total price of order
+ *               userData:
+ *                 type: object
+ *                 description: user data including phone, email
+ *     responses:
+ *       200:
+ *         description: Order successfully created
+ *       401:
+ *         description: Unauthorized. User is not authenticated.
+ */
+
+
+router.post('/', authMiddleware, createOrder);
+
+
+
+/**
+ * @swagger
+ * /api/orders:
+ *   get:
+ *     tags:
+ *       - orders
+ *     summary: Get user orders
+ *     description: Get user orders
+ *     security:
+ *       - BearerAuth: []
+ *     responses:
+ *       200:
+ *         description: array of user orders
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *       401:
+ *         description: Unauthorized. User is not authenticated.
+ */
+router.get('/', authMiddleware, getUserOrders);
+
+
+
+/**
+ * @swagger
+ * /api/orders:
+ *   delete:
+ *     tags:
+ *       - orders
+ *     summary: delete user order
+ *     description: delete user order
+ *     security:
+ *       - BearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Successfully deleted
+ *       401:
+ *         description: Unauthorized. User is not authenticated.
+ */
+router.delete('/', authMiddleware, deleteUserOrder)
 
 
 module.exports = {
