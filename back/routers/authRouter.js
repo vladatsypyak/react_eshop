2003b6be21842261ccfb8ad8a7c22b690303a6d1
Vladyslava <vladatsyupyak@gmail.com>
2024-06-19 18:56:56 +0300

    feat: test logs

--- back/routers/authRouter.js
+++ back/routers/authRouter.js
@@ -17,7 +17,10 @@ router.get("/login/failed", (req, res) => {
 router.get("/login/success", async (req, res) => {
     console.log("goes here")
     console.log(req)
+    console.log("passport from google" + " " + req.passport)
+
     console.log("user from google" + " " + req.user)
+
 
     if (req.user) {
         console.log(req.user.id)
