6aef6dce89fd4e737f6439d2e07648ea2d4739e2
Vladyslava <vladatsyupyak@gmail.com>
2024-06-23 20:59:07 +0300

    Revert "fix: cors origin url changed"
    This reverts commit 7fec5859376ef73189e4ca1ddc29e1ca306f5175.

--- back/index.js
+++ back/index.js
@@ -67,7 +67,7 @@ app.use(passport.initialize())
 app.use(passport.session())
 app.use(
     cors({
-        origin:"https://deft-boba-72d60a.netlify.app",
+        origin:"http://localhost:3000",
         methods: "GET,POST,PUT,DELETE",
         credentials: true
     })
