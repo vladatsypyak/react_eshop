f5e0f01a133fb3ed16a36d4fb21699b42b30d304
Vladyslava <vladatsyupyak@gmail.com>
2024-06-26 14:02:34 +0300

    fix: get port from the environment

--- back/index.js
+++ back/index.js
@@ -102,7 +102,7 @@ app.use(session({
 const start = async () => {
     try {
         console.log( `listening on ${apiUrl} `);
-        app.listen(process.env.PORT);
+        app.listen(process.env.PORT || 8080);
 
     } catch (err) {
         console.error(`Error on server startup: ${err.message}`);
