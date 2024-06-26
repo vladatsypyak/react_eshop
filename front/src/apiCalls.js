a111496df68f04195a6e367d4812620c117a5b62
Vladyslava <vladatsyupyak@gmail.com>
2024-06-26 15:58:09 +0300

    fix: replace url with variable

--- front/src/apiCalls.js
+++ front/src/apiCalls.js
@@ -1,7 +1,8 @@
 import {createAsyncThunk} from "@reduxjs/toolkit";
 import axios from "axios";
+import {apiUrl} from "./config";
 
 export const fetchFilters = createAsyncThunk('items/fetchFilters', async (params) => {
-    const {data} = await axios.get(`http://localhost:8080/api/app/filters/${params}`)
+    const {data} = await axios.get(`${apiUrl}/api/app/filters/${params}`)
     return data
 })
