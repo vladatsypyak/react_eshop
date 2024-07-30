3bbc8616b2b50545f0ef0d4a20997b3276e0b839
Vladyslava <vladatsyupyak@gmail.com>
2024-07-30 20:04:28 +0300

    fix: delete unnecessary code

--- back/helpers/helpers.js
+++ back/helpers/helpers.js
@@ -3,7 +3,6 @@ const jwt = require("jsonwebtoken");
 function getTokenPayload(req) {
     const {authorization} = req.headers;
     const [, token] = authorization.split(' ');
-    // const tokenPayload = jwt.verify(token, process.env.SECRET_KEY);
     return jwt.verify(token, "secret");
 }
 
